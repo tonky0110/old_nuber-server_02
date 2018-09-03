0aa56e835f8b05bca865a9dc5cd197b3097503e7
tonky0110 <tonky0110@tonky0110ui-MacBook-Air.local>
2018-09-03 21:38:25 +0900

    app options 세팅

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("work!");
+// 모듈 --> 라이브러리 순으로 import && 알파벳순서.
+import { Options } from 'graphql-yoga';
+import app from './app';
+
+const PORT : number | string = process.env.PORT || 4000;
+const PLAYGROUND_ENDPOINT : string = "/playground";
+const GRAPHQL_ENDPOINT : string = "/graphql";
+
+const appOptions : Options = {
+    port: PORT,
+    playground: PLAYGROUND_ENDPOINT,
+    endpoint: GRAPHQL_ENDPOINT
+};
+
+const handleAppStart = () => console.log(`Listening on port ${PORT}`);
+app.start(appOptions, handleAppStart);
