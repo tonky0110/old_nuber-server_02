e78ff907fe5bd514fb4ed9401c1ed9a279de618c
tonky0110 <tonky0110@gmail.com>
2018-09-09 00:53:01 +0900

    user, chat, message관계 설정.

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -7,9 +7,14 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn
+  
 } from "typeorm";
+import Chat from "./Chat";
+import Message from "./Message";
 
 const BCRYPT_ROUNDS = 10;
 
@@ -62,6 +67,12 @@ class User extends BaseEntity {
 
   @Column({ type: "double precision", default: 0 })
   lastOrientation: number;
+
+  @ManyToOne(type => Chat, chat => chat.participants)
+  chat: Chat;
+
+  @OneToMany(type => Message, message => message.user)
+  messages: Message[];
 
   @CreateDateColumn() createdAt: string;
 
--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,8 +1,3 @@
 export type verificationTarget = "PHONE" | "EMAIL";
 
-export type rideStatus =
-  | "ACCEPTED"
-  | "FINISHED"
-  | "CANCELED"
-  | "REQUESTING"
-  | "ONROUTE";
+export type rideStatus = "ACCEPTED" | "FINISHED" | "CANCLED"| "REQUESTING"| "ONROUTE";
