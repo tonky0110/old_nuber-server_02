096ed98b77505a631a3d9339ca0f4490ae3e713f
tonky0110 <tonky0110@gmail.com>
2018-09-17 23:02:49 +0900

    1-50. create email Verification(1/2)

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -4,3 +4,20 @@ const mailGunClient = new Mailgun({
     apiKey: process.env.MAILGUN_API_KEY || '',
     domain: "sandboxb09b86b031fa4bd5919993aef3936ee5.mailgun.org"
 });
+
+const sendEamil = (subject: string, html: string) => {
+    const emailData = { 
+        from: "tonky0110@gmail.com", 
+        to: "tonky0110@gmail.com", 
+        subject, 
+        html 
+    };
+
+    return mailGunClient.messages().send(emailData);
+};
+
+export const sendVerificationEmail = (fullName: string, key: string) => {
+    const emailSubject = `Hello ${fullName}, please verify you email`;
+    const emailBody = `Verify you email by clicking <a href="http://nuber.com/verification/${key}/">here</a>`;
+    return sendEamil(emailSubject, emailBody);
+}
--- src/utils/sendSMS.ts
+++ src/utils/sendSMS.ts
@@ -6,7 +6,7 @@ const twilioClient = Twilio(
 );
 
 
-export const sendSMS = (to: string, body: string) => {
+const sendSMS = (to: string, body: string) => {
     return twilioClient.messages.create({
         body,
         to,
