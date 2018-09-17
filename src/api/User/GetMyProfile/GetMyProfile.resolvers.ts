97e8539b9175c406695ef612cc13dec97b632669
tonky0110 <tonky0110@gmail.com>
2018-09-17 21:47:17 +0900

    js curry function의 매개변수로 function을 전달. 1번 function을 실행(체크)후 전달된 function을 실행.

--- src/api/User/GetMyProfile/GetMyProfile.resolvers.ts
+++ src/api/User/GetMyProfile/GetMyProfile.resolvers.ts
@@ -1,17 +1,16 @@
 import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
 
 const resolvers: Resolvers = {
-Query: {
-    GetMyProfile: async(_, __, {req}) => {
-        const { user } = req;
-        return {
-            ok: true,
-            error: null,
-            user
-
-        }
+    Query: {
+        GetMyProfile: privateResolver(async (_, __, { req }) => {
+            const { user } = req;
+            return {
+                ok: true,
+                error: null,
+                user
+            };
+        })
     }
-}
-}
-
+};
 export default resolvers;
