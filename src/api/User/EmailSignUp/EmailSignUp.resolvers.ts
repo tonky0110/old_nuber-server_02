484dec0f1d2cb3381315161eebfc4ceea73d0f05
tonky0110 <tonky0110@gmail.com>
2018-09-17 23:09:05 +0900

    1-51. create email Verification(2/3)

--- src/api/User/EmailSignUp/EmailSignUp.resolvers.ts
+++ src/api/User/EmailSignUp/EmailSignUp.resolvers.ts
@@ -3,6 +3,7 @@ import { Resolvers } from "../../../types/resolvers";
 import User from "../../../entities/User";
 import createJWT from "../../../utils/createJWT";
 import Verification from "../../../entities/Verification";
+import { sendVerificationEmail } from "../../../utils/sendEmail";
 
 const resolvers: Resolvers = {
     Mutation: {
@@ -20,18 +21,34 @@ const resolvers: Resolvers = {
                         token: null
                     };
                 }else {
-                    const newUser = await User.create({ ...args }).save();
-                    if(newUser.email){
-                        const emailVerification = await Verification.create({
-                            payload: newUser.email,
-                            target: "EMAIL"
-                        });
-                    }
-                    const token = createJWT(newUser.id);
-                    return {
-                        ok: true,
-                        error: null,
-                        token
+                    const phoneVerification = await Verification.findOne({
+                        payload: args.phoneNumber, 
+                        verified: true
+                    });
+                    if(phoneVerification){
+                        const newUser = await User.create({ ...args }).save();
+                        if (newUser.email) {
+                            const emailVerification = await Verification.create({
+                                payload: newUser.email,
+                                target: "EMAIL"
+                            });
+                            await sendVerificationEmail(
+                                newUser.fullName, 
+                                emailVerification.key
+                            );
+                        }
+                        const token = createJWT(newUser.id);
+                        return {
+                            ok: true,
+                            error: null,
+                            token
+                        }
+                    }else {
+                        return {
+                            ok: false,
+                            error: "You haven't verified you phone number.",
+                            token: null
+                        };
                     }
                 }
             }catch(error) {
